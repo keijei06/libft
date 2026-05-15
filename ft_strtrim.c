de4a6783f3100cf72827c2eb509c0bc9367b320b
keijei06 <jcbkito.avila@hotmail.com>
2026-05-15 17:00:35 +0100

    added

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,9 +6,11 @@
 /*   By: joborges <joborges@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 15:20:36 by joborges          #+#    #+#             */
-/*   Updated: 2026/05/12 22:20:30 by joborges         ###   ########.fr       */
+/*   Updated: 2026/05/15 16:54:55 by joborges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 #include "libft.h"
 
@@ -16,27 +18,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
 	int		j;
-	int		i_alloc;
+	int		k;
 	char	*alloc;
-	
+
 	if (!s1 || !set)
 		return (NULL);
+	if (*s1 == '\0')
+		return (ft_strdup(""));
 	i = 0;
-	i_alloc = 0;
-	j = ft_strlen(s1) - 1;
-	while (ft_strchr(set, s1[i]) != NULL)
+	j = (int)ft_strlen(s1) - 1;
+	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	while (j > i && ft_strchr(set, s1[j]) != NULL)
+	while (j > i && ft_strchr(set, s1[j]))
 		j--;
 	alloc = malloc(j - i + 2);
 	if (!alloc)
 		return (NULL);
+	k = 0;
 	while (i <= j)
-	{
-		alloc[i_alloc] = s1[i];
-		i++;
-		i_alloc++;
-	}
-	alloc[i_alloc] = '\0';
+		alloc[k++] = s1[i++];
+	alloc[k] = '\0';
 	return (alloc);
 }
